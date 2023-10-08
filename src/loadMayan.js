9c808f0debd933c630fc8a3e0e43abf0041d4642
KiT O <keykakito@gmail.com>
2023-10-08 16:10:41 +0330

    fix integrity issue

--- src/loadMayan.js
+++ src/loadMayan.js
@@ -8,7 +8,7 @@ export default async function loadMayan() {
   const script = document.createElement('script')
   script.src = "https://cdn.mayan.finance/widget_ultimate-0-4-5.js"
   script.crossorigin = "anonymous"
-  // script.integrity = 'sha256-Dem40VAlLsczlbgJyd9U20HCZiihA1UFQy96wdDqVYQ='
+  if (window.location.protocol === "https:") script.integrity = 'sha256-Dem40VAlLsczlbgJyd9U20HCZiihA1UFQy96wdDqVYQ='
   document.body.appendChild(script)
 
   loading = new Promise((resolve, reject) => {
